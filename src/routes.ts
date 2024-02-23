ac3b4c85efd6f50700dec2292d2c21a30855e733
Bruno Fabre <bruno.hfabre@gmail.com>
2024-02-23 15:02:20 -0300

    remove redis

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
-import { clear } from './http/clear'
 import { importList } from './http/import-list'
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(importList)
-
-  app.register(clear)
 }
